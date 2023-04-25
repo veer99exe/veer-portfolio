1315f53bcaf15fa09dc28f7043f3e8c52023a64b
veer99exe <veerbhadragohil@gmail.com>
2023-04-25 18:23:25 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import About from "./components/About";
+import Contact from "./components/Contact";
+import Experience from "./components/Experience";
+import Home from "./components/Home";
+import NavBar from "./components/NavBar";
+import SocialLinks from "./components/SocialLinks";
+
+function App() {
+  return (
+    <div>
+      <NavBar />
+      <Home />
+      <About />
+      <Experience />
+      <Contact />
+      <SocialLinks />
+    </div>
+  );
+}
+
+export default App;
